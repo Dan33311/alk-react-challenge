e6c0d940b4eaefc7a0b927e40c24b5a4f06b3efb
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-05-03 16:27:42 -0500

    Login component with validations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Login from "./components/Login";
 
 
 function App() {
   return (
     <div className="App">
-      <h2>App working ...</h2>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+const Login = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    if(email === '' || password === ''){
+      console.log('there is empty fields');
+      return
+    }
+
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if(email !== '' && !regexEmail.test(email)){
+      console.log('invalid email format');
+      return
+    }
+
+    if(email !== 'challenge@alkemy.org' || password !== 'react'){
+      console.log('invalid credentials');
+      return
+    }
+
+    console.log('success, we are ready for the next step');
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Email:</span><br />
+          <input type="text" name="email" />
+        </label>
+        <br />
+
+        <label>
+          <span>Password:</span><br />
+          <input type="password" name="password" />
+        </label>
+        <br />
+
+        <button>Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
